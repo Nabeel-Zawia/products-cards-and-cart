7ed2d738622e416e99329146e5da26e063655113
Nabeel Khalid Zawia <devnabeelkhalid@gmail.com>
2025-10-14 04:39:24 +0300

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const purchaseButtons = document.querySelectorAll('.button-middle');
+  const productTitles = document.querySelectorAll('.product-title');
+  const productPrice = document.querySelectorAll('.product-price');
+  const cart = document.getElementById("cart");
+
+  const cartObject = {
+    products: []
+  };
+
+
+  purchaseButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+      const product = {
+        name: productTitles[index].textContent,
+        price: productPrice[index].textContent
+      };
+
+      cartObject.products.push(product);
+      saveData(cartObject);
+    });
+  });
+
+
+  function saveData(data) {
+    localStorage.setItem("products", JSON.stringify(data));
+  }
+
+
+  cart.addEventListener('click', function (e) {
+    e.preventDefault();        
+    window.location.href = "cart.html";
+  });
+
+
+});
